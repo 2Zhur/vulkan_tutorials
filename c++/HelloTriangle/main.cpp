50915cfc990fc9b3065f733794173664d0ea9a05
2Zhur <mkzhurav@gmail.com>
2022-03-16 17:24:59 -0400

    Successfully created a Vulkan instance

--- c++/HelloTriangle/main.cpp
+++ c++/HelloTriangle/main.cpp
@@ -1,13 +1,112 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+/**
+ * A quick side note on code styling: I prefer to use
+ * snake-case instead of camelback-case. It is consistent
+ * with the style of the standard library and I consider it
+ * to be more aesthetically pleasing. As a bonus, it allows
+ * to better differenciate between API calls and my own code
+ */
+
+class hello_triangle_application
+{
+public:
+    void run() {
+        init_window();
+        init_vulkan();
+        main_loop();
+        cleanup();
+    }
+
+private:
+    void init_window() {
+        glfwInit();
+        // tell GLFW not to create an OpenGL context
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // disable window resizing for now
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        _window = glfwCreateWindow(_width, _hight, "Vulkan", nullptr, nullptr);
+
+    }
+
+    void create_instance() {
+        /* Fill out application information for the Vulkan instance */
+        VkApplicationInfo app_info{};
+        app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        app_info.pApplicationName = "Hello Triangle";
+        app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        app_info.pEngineName = "No Engine";
+        app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        app_info.apiVersion = VK_API_VERSION_1_0;
+
+        /* Fill out Vulkan instance information */
+        VkInstanceCreateInfo create_info{};
+        create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        create_info.pApplicationInfo = &app_info;
+
+        /// Right now we will only need an extension to interface with
+        /// the application window, which is done through GLFW
+        uint32_t glfw_extesion_count = 0;
+        const char** glfw_extensions;
+
+        // request necessary extensions from GLFW
+        glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extesion_count);
+        // add extension info appInfofor the Vulkan instance 
+        create_info.enabledExtensionCount = glfw_extesion_count;
+        create_info.ppEnabledExtensionNames = glfw_extensions;
+        // more about validation layers later; for now just plug a zero
+        create_info.enabledLayerCount = 0;
+
+        /* Create the Vulkan instance */
+        // NOTE: custom allocator callbacks will not be used throughout
+        //       this tutorial, and for this reason the second argument
+        //       will always be nullptr.
+        if (vkCreateInstance(&create_info, nullptr, &_instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create a Vulkan instance!");
+        }
+
+
+        /*  */
+    }
+
+    void init_vulkan() {
+        create_instance();
+    }
+
+    void main_loop() {
+        while (!glfwWindowShouldClose(_window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(_window);
+        glfwTerminate();
+    }
+
+    // Private members
+    VkInstance _instance;
+
+    GLFWwindow* _window;
+    const uint32_t _width = 800;
+    const uint32_t _hight = 600;
+};
 
 int main() {
-    
+    hello_triangle_application app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
